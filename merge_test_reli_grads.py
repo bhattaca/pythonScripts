92ee85154cfc4a42a76b7ba202374ffff9b68950
Arindam Bhattacharya <bhattacharya.29@osu.edu>
2014-12-03 17:46:14 -0500

    Data and python

--- merge_test_reli_grads.py
+++ merge_test_reli_grads.py
@@ -10,7 +10,7 @@ import os
 #configurations
 
 #setup isovalues
-isoval_opts =['3.7']
+isoval_opts =['4.1']
 
 #set up location of file locations
 fread = open ('./file-names.txt','r') # contains the names of the files on which the test is run
@@ -31,19 +31,19 @@ computeGradTests=[]
 #	TESTS
 ######################################################################
 
+
 mergesharp = ['E:\Programs\Win\stdAlone\mergesharp_bin\Release\mergesharp.exe','-trimesh', '-max_grad_dist', '4',
               '-gradS_offset', '1', '-max_dist', '1.0', '-position', 'gradBIES', '-map_extended' ]
 mergesharpTests.append(mergesharp)
-
 ######################################################################
 #COMPUTE GRAD TESTS
 ######################################################################
 
 computeGrad = ['E:\Programs\Win\stdAlone\\religrad_bin\\Release\\religrad.exe',  '-angle_test',  '-scalar_test']
-#computeGradTests.append(computeGrad)
-
-computeGrad = ['E:\Programs\Win\stdAlone\\religrad_bin\\Release\\religrad.exe',  '-advangle']
 computeGradTests.append(computeGrad)
+
+#computeGrad = ['E:\Programs\Win\stdAlone\\religrad_bin\\Release\\religrad.exe',  '-curvature_based']
+#computeGradTests.append(computeGrad)
 
 '''
 #test1
@@ -72,7 +72,7 @@ mergesharpTests.append(mergesharp)
 #######################################################################
                         
 findsharp_windows = ['E:\\Programs\Win\\stdAlone\\findsharp_bin\\Debug\\findsharp.exe','140']
-countdegree_windows =['E:\\Programs\\Win\\stdAlone\\findEdgeCount_bin\\Debug\\findEdgeCount.exe', '-fp']
+countdegree_windows =['E:\\Programs\Win\\stdAlone\\count_degree_bin\\Debug\\countdegree.exe', '-fshort','-e']
 
 '''
 for each file run mergesharp with correct gradients
@@ -83,15 +83,16 @@ def test_correct_grad(n,iso):
     for f in fread:
         fname_with_nrrd = f.split("/")[len(f.split("/"))-1]
         fname = fname_with_nrrd.split(".")[0]
+        print ("\n**********************************\n filename ", fname)
         fgradnoisename = fname+".grad.nrrd"
         for i in isoval_opts:
                 #iso_temp = isodual3D[:]
                 iso_temp = iso[:]        
-                test_details = str(n)+","+fname+","+str(i)
-                n = n+1
+                test_details = str(n)+","+fname+","+str(i)+"\n"
+                print("isovalue ", i)
                 #fname_line_name = loc+"output_offs/"+fname+"-iso-"+str(i)+".line"
                 iso_temp.append('-o')
-                iso_temp.append('out.off')
+                iso_temp.append("out_ms"+str(n)+".off")
                 iso_temp.append('-s')
                 #add the gradient file name extracted from the file name.
                 iso_temp.append('-gradient')
@@ -99,18 +100,19 @@ def test_correct_grad(n,iso):
                 iso_temp.append(i)
                 iso_temp.append(f.strip())
                 #print "test call:", iso_temp[:]
-                print >>ftestdetails, test_details, iso, "correct"
+                #print >>ftestdetails, test_details, iso, "correct"
+                ftestdetails.write(test_details)
                 procced = proc.check_call(iso_temp)
                 
                 if procced==0:
                     if procced == 0:
                         findsharpTemp = findsharp_windows[:]
-                        findsharpTemp.append("out.off")
+                        findsharpTemp.append("out_ms"+str(n)+".off")
                         #print "findsharp: ", findsharpTemp[:]
                         procced = proc.check_call(findsharpTemp)
                         if procced ==0:
                             countdegreeTemp = countdegree_windows[:]
-                            countdegreeTemp.append("out.line")
+                            countdegreeTemp.append("out_ms"+str(n)+".line")
                             #print "countdegree: ", countdegreeTemp[:]
                             procced=proc.check_call(countdegreeTemp,stdout=fedgecount)
                         else:
@@ -137,16 +139,16 @@ def test__grad(n, m, g, mt):
         #gradients
         temp_g = g[:]
         temp_g.append(f.strip())
-        fgrad_name = fname+".grad.nrrd"
+        fgrad_name = fname+".test.grad.nrrd"
         temp_g.append(fgrad_name)
         #print(temp_g)
         procced = proc.check_call(temp_g)
         
         for s in isoval_opts:
             mt_temp = mt[:]
-            test_details = str(n)+","+str(m)+","+fname+"\n"# test details
+            test_details = "cg "+str(n)+",ms "+str(m)+","+fname+","+str(s)+"\n"# test details
             mt_temp.append('-o')
-            mt_temp.append('out.off')
+            mt_temp.append("out_cg"+str(n)+"ms"+str(m)+".off")
             mt_temp.append('-s')
             #add the gradient file name extracted from the file name.
             mt_temp.append('-gradient')
@@ -159,12 +161,12 @@ def test__grad(n, m, g, mt):
             procced = proc.check_call(mt_temp)
             
             findsharpTemp = findsharp_windows[:]
-            findsharpTemp.append("out.off")
+            findsharpTemp.append("out_cg"+str(n)+"ms"+str(m)+".off")
             #print "findsharp: ", findsharpTemp[:]
             procced = proc.check_call(findsharpTemp)
             
             countdegreeTemp = countdegree_windows[:]
-            countdegreeTemp.append("out.line")
+            countdegreeTemp.append("out_cg"+str(n)+"ms"+str(m)+".line")
             #print "countdegree: ", countdegreeTemp[:]
             procced=proc.check_call(countdegreeTemp,stdout=fedgecount)
 
@@ -173,14 +175,20 @@ def main():
     print ("status: start test1")
     n=1
     m=1
+	
+    for mt in mergesharpTests:
+        test_correct_grad(m,mt)
+        m=m+1
+        
     for g in computeGradTests:
+        #ms
         m=1
         for mt in mergesharpTests:
            # print iso
-           print ("\nstatus: in function MAIN\n ", mt, g)
-           #print >>ftestdetails, iso 
-           test__grad(n, m, g, mt) 
-           m=m+1
+            print ("\nstatus: in function MAIN\n ", mt, g)
+            #print >>ftestdetails, iso
+            #test__grad(n, m, g, mt) 
+            m=m+1
         n=n+1;
 if __name__ == "__main__":
     main()            
